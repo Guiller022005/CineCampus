7fe5669f22580db036d1d1d09403d62f92316574
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-09-02 15:34:16 -0500

    feat: :sparkles: Api para listar Usuarios por rol 'VIP' o 'estandar'

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -19,3 +19,27 @@ exports.obtenerUsuarios = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
+/**
+ * Controlador para obtener usuarios filtrados por rol.
+ *
+ * @async
+ * @function obtenerUsuariosByRol
+ * @param {Object} req - Objeto de solicitud (request) value define el rol a filtrar 'VIP' o 'estandar' descomenta el q necesitas usar .
+ * @param {Object} res - Objeto de respuesta (response).
+ * @returns {void} - Devuelve una respuesta JSON con los usuarios filtrados por rol o un mensaje de error.
+ */
+
+
+exports.obtenerUsuariosByRol = async (req, res) => {
+    try {
+        // const value = 'VIP';
+        const value = 'estandar';
+        const usuarios = await user.find({ rol: value }); // Obtén todas las películas
+        console.log('Usuarios obtenidas:', usuarios); // Imprime en consola para depuración
+        res.status(200).json(usuarios); // Devuelve las películas en formato JSON
+    } catch (error) {
+        console.error('Error al obtener usuarios:', error); // Imprime el error en consola
+        res.status(500).json({ error: error.message }); // Devuelve un error si ocurre
+    }
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -5,5 +5,7 @@ const usersController = require('../controllers/usersController');
 
 // Ruta para obtener todos los usuarios
 router.get('/', usersController.obtenerUsuarios);
+// Ruta para buscar usuarios por su rol 'VIP' o 'estandar'
+router.get('/rol', usersController.obtenerUsuariosByRol);
 
 module.exports = router;
